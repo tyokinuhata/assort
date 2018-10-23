cedac94b912d36d12d796b6fcd3a322004ad3b5b
NNN <b7080@oic.jp>
2018-10-23 16:57:00 +0900

    db デフォルト文字コードをUTF8に

--- config/database.php
+++ config/database.php
@@ -41,14 +41,15 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+						'unix_socket'   => env('DB_SOCKET', null),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset' => 'utf8',
+            'collation' => 'utf8_general_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
