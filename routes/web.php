e6e4f803189ca83ae1d71ded40449a5833de9871
NNN <b7080@oic.jp>
2018-11-06 15:07:02 +0900

    仮ルーティングを設定

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,49 @@ Route::get('/product_search', 'ProductsController@index');
 Route::get('/product_detail', 'ProductsController@show');
 
 //注文管理
-Route::get('/recieved', 'RecievedController@index');
+Route::get('/recieved_search', 'RecievedController@index');
+
+//出金管理
+Route::get('/payment_search', function () {
+	return view('payment/payment_search');
+});
+
+//入金管理
+Route::get('/spending_search', function () {
+	return view('spendging/spending_search');
+});
+
+//売上管理
+Route::get('/sales_search', function () {
+	return view('sales/sales_search');
+});
+
+//カテゴリ管理
+Route::get('/category_search', function () {
+	return view('category/category_search');
+});
+
+//ブランド管理
+Route::get('/brand_search', function () {
+	return view('brand/brand_search');
+});
+//顧客管理
+Route::get('/customer_search', function () {
+	return view('customer/customer_search');
+});
+//従業員管理
+Route::get('/staff', function () {
+	return view('staff/staff_search');
+});
+//色管理
+Route::get('/color_search', function () {
+	return view('color/color_search');
+});
+//サイズ管理
+Route::get('/size_search', function () {
+	return view('size/size_search');
+});
+//棚番号管理
+Route::get('/stock_shelf', function () {
+	return view('stock_shelf/stock_shelf_search');
+});
