087a818d40e7ab77cb7d622fd278be16cddde247
NNN <b7080@oic.jp>
2018-11-06 14:34:51 +0900

    メニューリンクを*_searchに変更

--- resources/views/layout/side.blade.php
+++ resources/views/layout/side.blade.php
@@ -15,36 +15,36 @@
         </div>
     </div>
     <div class="menu_item">
-        <a href="/recieved" class="menu_block_name">注文管理</a>
+        <a href="/recieved_search" class="menu_block_name">注文管理</a>
     </div>
     <div class="menu_item">
         <a href="" class="menu_block_name">発送管理</a>
     </div>
     <div class="menu_item">
-        <a href="/payment" class="menu_block_name">入金管理</a>
+        <a href="/payment_search" class="menu_block_name">入金管理</a>
     </div>
     <div class="menu_item">
-        <a href="/spending" class="menu_block_name">出金管理</a>
+        <a href="/spending_search" class="menu_block_name">出金管理</a>
     </div>
     <div class="menu_item">
-        <a href="/sales" class="menu_block_name">売上管理</a>
+        <a href="/sales_search" class="menu_block_name">売上管理</a>
     </div>
     <div class="menu_item">
         <a class="menu_block_name has_submenu">カテゴリ管理</a>
         <div class="menu_block_hiden">
-            <a href="/category" class="menu_block_subitem">子カテゴリ管理</a>
-            <a href="/parent_category" class="menu_block_subitem">親カテゴリ管理</a>
+            <a href="/category_search" class="menu_block_subitem">子カテゴリ管理</a>
+            <a href="/parent_category_search" class="menu_block_subitem">親カテゴリ管理</a>
         </div>
     </div>
     <div class="menu_item">
         <a class="menu_block_name has_submenu">マスター管理</a>
         <div class="menu_block_hiden">
-            <a href="/brand" class="menu_block_subitem">仕入先管理</a>
-            <a href="/customer" class="menu_block_subitem">顧客管理</a>
-            <a href="/staff" class="menu_block_subitem">従業員管理</a>
-            <a href="/color" class="menu_block_subitem">色管理</a>
-            <a href="/size" class="menu_block_subitem">サイズ管理</a>
-            <a href="/shelf" class="menu_block_subitem">棚番号管理</a>
+            <a href="/brand_search" class="menu_block_subitem">仕入先管理</a>
+            <a href="/customer_search" class="menu_block_subitem">顧客管理</a>
+            <a href="/staff_search" class="menu_block_subitem">従業員管理</a>
+            <a href="/color_search" class="menu_block_subitem">色管理</a>
+            <a href="/size_search" class="menu_block_subitem">サイズ管理</a>
+            <a href="/shelf_search" class="menu_block_subitem">棚番号管理</a>
         </div>
     </div>
 </div>
