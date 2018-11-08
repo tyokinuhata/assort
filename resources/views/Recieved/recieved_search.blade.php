c678a571d796bbef4b14a596c7e5b8839bb7d7bc
CreateAnything434 <b7067@oic.jp>
2018-11-08 13:05:59 +0900

    First 電話番号記述済み

---
+++ resources/views/Recieved/recieved_search.blade.php
@@ -0,0 +1,73 @@
+@extends('layout.common')
+@section('title', '注文確認画面')
+
+@section('content')
+    <div class="main_content">
+        <h1 id="title">注文確認画面</h1>
+
+        <table>
+            <tbody>
+            <tr>
+                <th colspan="2">注文内容</th>
+
+            </tr>
+            <tr>
+                <th class="left">注文番号</th>
+
+
+                <td></td>
+            </tr>
+            <tr>
+                <th class="left">注文日</th>
+                <td></td>
+            </tr>
+            　<tr>
+                <th class="left">注文者</th>
+                <td><input class="form-control" type="text" name="" size="40" maxlength="40" value="" placeholder="OIC 太郎"></td>
+            </tr>
+            　<tr>
+                <th class="left">発送日</th>
+                <td></td>
+            </tr>
+            　<tr>
+                <th class="left">お届け先</th>
+                <td></td>
+            </tr>
+            　<tr>
+                <th class="left">電話番号</th>
+                <td class="row">
+                    <div class="col-xs-3"><input class="form-control" type="tel" name="" size="4" maxlength="4" value="" placeholder="0000"></div><div class="hyphen">-</div>
+                    <div class="col-xs-3"><input class="form-control" type="tel" name="" size="4" maxlength="4" value="" placeholder="1111"></div><div class="hyphen">-</div>
+                    <div class="col-xs-3"><input class="form-control" type="tel" name="" size="4" maxlength="4" value="" placeholder="2222"></div>
+                </td>
+            </tr>
+            　<tr>
+                <th class="left">支払い方法</th>
+                <td></td>
+            </tr>
+            　<tr>
+                <th class="left">支払い金額</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th class="left">更新者</th>
+                <td></td>
+            </tr>
+            <tr>
+                <th class="left">更新日</th>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
+        <div align="right">
+            <a href="#" class="square_btn">
+                <i class="fa fa-caret-right"></i>登録</a>
+            <a href="#" class="reseto_btn">
+                <i class="fa fa-caret-right"></i>リセット</a>
+            <a href="#" class="update_btn">
+                <i class="fa fa-caret-right"></i>更新</a>
+            <a href="#" class="delete_btn">
+                <i class="fa fa-caret-right"></i>削除</a>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/arrival/arrival_search.blade.php
@@ -0,0 +1,54 @@
+@extends('layout.common')
+@section('title', '入庫管理')
+
+@section('content')
+<div class="main_content">
+	<h1 id="title">入庫管理画面</h1>
+
+	<table>
+		<tbody>
+		<tr>
+			<th colspan="2">入庫情報</th>
+		</tr>
+		<tr>
+		<th class="left">入庫場所コード</th>
+		<td></td>
+		</tr>
+		<tr>
+			<th class="left">棚番号</th>
+			<td>X-00</td>
+		</tr>
+	　<tr>
+			<th class="left">商品コード</th>
+			<td><input type="text" name="" size="20" maxlength="40" value="" placeholder="商品コード"></td>
+		</tr>
+	　<tr>
+			<th class="left">入庫数量</th>
+			<td>3</td>
+		</tr>
+	　<tr>
+			<th class="left">入庫日時</th>
+			<td><input type="date" name="date" value="2018-01-01"></td>
+		</tr>
+		<tr>
+			<th class="left">最終更新者</th>
+			<td><input type="text" name="" size="40" maxlength="40" value="" placeholder="OIC 太郎"></td>
+		</tr>
+		<tr>
+			<th class="left">最終更新日</th>
+			<td><input type="date" name="date" value="2018-01-01"></td>
+		</tr>
+		</tbody>
+	</table>
+	<div align="right">
+	<a href="#" class="square_btn">
+		<i class="fa fa-caret-right"></i>登録</a>
+	<a href="#" class="reseto_btn">
+		<i class="fa fa-caret-right"></i>リセット</a> 
+	<a href="#" class="update_btn">
+		<i class="fa fa-caret-right"></i>更新</a>
+	<a href="#" class="delete_btn">
+		<i class="fa fa-caret-right"></i>削除</a>
+	</div>
+</div>
+@endsection
