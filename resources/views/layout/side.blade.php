060e2ae6017ae27094f6026015100e33110251c9
kakimoto-S <b7080@oic.jp>
2018-11-09 14:20:58 +0900

    Revert "Feature/146"

--- resources/views/layout/side.blade.php
+++ resources/views/layout/side.blade.php
@@ -1,54 +1,4 @@
 <div class="side_menu">
-<<<<<<< HEAD
-	<div class="menu_item">
-		<a href="/order_search" class="menu_block_name">発注管理</a>
-	</div>
-	<div class="menu_item">
-		<a href="/arrival_search" class="menu_block_name">入庫管理</a>
-	</div>
-	<div class="menu_item">
-		<a class="menu_block_name has_submenu">商品管理</a>
-		<div class="menu_block_hiden">
-			<a href="/product_search" class="menu_block_subitem">商品リスト</a>
-			<a href="/inventory" class="menu_block_subitem">棚卸し</a>
-			<a href="/waste" class="menu_block_subitem">不良品登録</a>
-			<a href="/stock_shelf_change" class="menu_block_subitem">棚配置変更</a>
-		</div>
-	</div>
-	<div class="menu_item">
-		<a href="/recieved" class="menu_block_name">注文管理</a>
-	</div>
-	{{--<div class="menu_item">--}}
-		{{--<a href="" class="menu_block_name">発送管理</a>--}}
-	{{--</div>--}}
-	<div class="menu_item">
-		<a href="/payment" class="menu_block_name">入金管理</a>
-	</div>
-	<div class="menu_item">
-		<a href="/spending" class="menu_block_name">出金管理</a>
-	</div>
-	<div class="menu_item">
-		<a href="/sales" class="menu_block_name">売上管理</a>
-	</div>
-	<div class="menu_item">
-		<a class="menu_block_name has_submenu">カテゴリ管理</a>
-		<div class="menu_block_hiden">
-			<a href="/category" class="menu_block_subitem">子カテゴリ管理</a>
-			<a href="/parent_category" class="menu_block_subitem">親カテゴリ管理</a>
-		</div>
-	</div>
-	<div class="menu_item">
-		<a class="menu_block_name has_submenu">マスター管理</a>
-		<div class="menu_block_hiden">
-			<a href="/brand" class="menu_block_subitem">仕入先管理</a>
-			<a href="/customer" class="menu_block_subitem">顧客管理</a>
-			<a href="/staff" class="menu_block_subitem">従業員管理</a>
-			<a href="/color" class="menu_block_subitem">色管理</a>
-			<a href="/size" class="menu_block_subitem">サイズ管理</a>
-			<a href="/shelf" class="menu_block_subitem">棚番号管理</a>
-		</div>
-	</div>
-=======
     <div class="menu_item">
         <a href="/order_search" class="menu_block_name">発注管理</a>
     </div>
@@ -97,5 +47,4 @@
             <a href="/shelf" class="menu_block_subitem">棚番号管理</a>
         </div>
     </div>
->>>>>>> c678a571d796bbef4b14a596c7e5b8839bb7d7bc
 </div>
