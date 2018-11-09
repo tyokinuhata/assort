7b6973ab6eaa52a1a7b6bb4619a4902ae09df7e9
山本蓮 <yamamotoren@yamamotorennoMacBook-ea.local>
2018-11-09 16:26:15 +0900

    139,ビューにcss反映.

---
+++ resources/views/sales/sales_search.blade.php
@@ -0,0 +1,36 @@
+@extends('layout.common')
+@section('title', '売上管理画面')
+@section('header_title','売上管理画面')
+
+@section('content')
+	<div class="content_wrapper">
+        <table class="table-bordered">
+		  <tbody>
+			<tr>
+				<th colspan="2">売上情報</th>
+			</tr>
+			<tr>
+			<th class="left">期間開始日</th>
+			<td><input class="form-control" type="date" name="date" value="" placeholder="2018-01-01"></td>
+			</tr>
+		　<tr>
+				<th class="left">期間終了日</th>
+				<td><input class="form-control" type="date" name="date" value="" placeholder="2018-01-01"></td>
+			</tr>
+		　<tr>
+				<th class="left">総利益</th>
+				<td></td>
+			</tr>
+		　<tr>
+				<th class="left">売上純利益</th>
+				<td></td>
+			</tr>
+		　<tr>
+				<th class="left">売上計上日</th>
+				<td></td>
+			</tr>
+		</tbody>
+		</table>
+		<img src="images/graph.png" alt="折れ線グラフ" width="70%">
+	</div>
+@endsection
